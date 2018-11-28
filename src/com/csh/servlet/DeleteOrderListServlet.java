96a260b9941f9ba79c209ffbd1f711b023fb75af
loveml505 <loveml505@163.com>
2018-11-28 14:25:48 +0800

    使用idea提交

---
+++ src/com/csh/servlet/DeleteOrderListServlet.java
@@ -0,0 +1,22 @@
+package com.csh.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by admin on 2018/11/28.
+ */
+@WebServlet(name = "DeleteOrderListServlet")
+public class DeleteOrderListServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
